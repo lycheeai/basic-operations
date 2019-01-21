1a261c79b5167b60b86f43197e96d24259cecdb3
Jieren Chen <jieren.chen@gmail.com>
2019-01-22 06:50:52 +0700

    add real code to import_table

--- import_table.py
+++ import_table.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 def import_table(file_gen):
-  pd.read_csv(next(file_gen))
-  
-  yield 
+  first_file = next(file_gen)
+  pd_csv = pandas.read_csv(first_file.result)
+  def extract_column(csv, col_name):
+    (PipelineSuccess(first_file.name + "/" + k, v.encode("utf8")) for k, v in enumerate(my_csv['id']))
+  return [extract_column(pd_csv, i) for i in pd_csv.columns]
