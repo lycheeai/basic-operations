c610d1ed92053461cec35db31c988972d5a13574
Jieren Chen <jieren.chen@gmail.com>
2019-01-22 06:57:27 +0700

    fix

--- import_table.py
+++ import_table.py
@@ -4,5 +4,5 @@ def import_table(file_gen):
   first_file = next(file_gen)
   pd_csv = pd.read_csv(first_file.result)
   def extract_column(csv, col_name):
-    return (PipelineSuccess(first_file.name + "/" + k, v.encode("utf8")) for k, v in enumerate(csv[col_name]))
+    return (PipelineSuccess(first_file.name + "/" + str(k), v.encode("utf8")) for k, v in enumerate(csv[col_name]))
   return [extract_column(pd_csv, i) for i in pd_csv.columns]
