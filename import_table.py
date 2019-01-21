490010c62e07e4acbbea22e769c7c69f65e94f35
Jieren Chen <jieren.chen@gmail.com>
2019-01-22 06:53:07 +0700

    fix

--- import_table.py
+++ import_table.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def import_table(file_gen):
   first_file = next(file_gen)
-  pd_csv = pandas.read_csv(first_file.result)
+  pd_csv = pd.read_csv(first_file.result)
   def extract_column(csv, col_name):
     (PipelineSuccess(first_file.name + "/" + k, v.encode("utf8")) for k, v in enumerate(my_csv['id']))
   return [extract_column(pd_csv, i) for i in pd_csv.columns]
