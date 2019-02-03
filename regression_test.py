b6fcda8889af7dfb45813b052321d5c4afe909a4
Jieren Chen <jieren.chen@gmail.com>
2019-02-03 12:28:54 +0800

    lol oops

--- regression_test.py
+++ regression_test.py
@@ -2,10 +2,11 @@ import pandas as pd
 import statsmodels.formula.api as sm
 
 def regression_test(a, b, reg_name):
-  {
+  base = {
     'a': [i.result for i in a],
     'b': [i.result for i in b]
   }
+  df = pd.DataFrame(base)
   normal_ols = sm.ols(formula='a ~ b',
                           data=df).fit()
   yield PipelineSuccess(reg_name, normal_ols.summary())
